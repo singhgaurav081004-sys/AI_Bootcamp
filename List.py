f0ee7f3f98e50cbfe862db1aaa772c637bade6b4
singhgaurav081004-sys <singhgaurav081004@gmail.com>
2026-09-21 23:14:50 +0530

    First commit

---
+++ List.py
@@ -0,0 +1,6 @@
+import random
+
+numbers = [random.randint(1, 100) for _ in range(20)]
+
+print("List:", numbers)
+print("Even numbers:", [num for num in numbers if num % 2 == 0])
---
+++ welcome.py
@@ -0,0 +1,1 @@
+print("welcome srm students in the session of bootcamp")
